776987a4fc97504827bfaeb0e03d01d9d208e648
Arman Safaryan <arman_safaryan@edu.aua.am>
2018-09-13 15:49:37 +0400

    Hello

---
+++ hw2.js
@@ -0,0 +1,11 @@
+const a = 30;
+const b = 10;
+const c = 8;
+const d = "Thomas";
+const e = "Payne";
+
+if ((a-b+c===28 && b+c===20) ||(d+e !== "PayneThomas")) {
+console.log("Hello AUA");
+}
+else {console.log("Bye AUA");
+}
